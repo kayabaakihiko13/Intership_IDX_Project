20d97aa3db00699f9cfe47855978df872a649316
kayabaakihiko13 <iqbalramad75@gmail.com>
2023-11-27 03:53:33 +0700

    feat:update algoritm

--- Charlotte_test/explor_visual_test.py
+++ Charlotte_test/explor_visual_test.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from Charlotte.explor_visuals import Countplot_Visual,PiePlot
+from Charlotte.explor_visuals import Countplot_Visual, PiePlot
 
 
 class TestCountplotVisual(unittest.TestCase):
@@ -31,8 +31,8 @@ class TestCountplotVisual(unittest.TestCase):
         plot = Countplot_Visual(data_df)
         self.assertIsInstance(plot, plt.Figure)
 
-class TestPiePlotFunction(unittest.TestCase):
 
+class TestPiePlotFunction(unittest.TestCase):
     def setUp(self):
         # Set up any necessary data for testing
         self.data = pd.Series([1, 2, 3, 1, 2, 1, 3, 3])
@@ -46,7 +46,7 @@ class TestPiePlotFunction(unittest.TestCase):
         # Test with title parameter
         fig = PiePlot(self.data, title="Test Title")
         self.assertIsInstance(fig, plt.Figure)
-        
+
 
 if __name__ == "__main__":
     unittest.main()
--- Charlotte_test/feature_selection_test.py
+++ Charlotte_test/feature_selection_test.py
@@ -1,16 +1,20 @@
 import unittest
 import pandas as pd
 import numpy as np
-from Charlotte.feature_selection import selection_by_variance,pearson_correlation_selection
-class TestFeatureSelectionMethods(unittest.TestCase):
+from Charlotte.feature_selection import (
+    selection_by_variance,
+    pearson_correlation_selection,
+)
 
+
+class TestFeatureSelectionMethods(unittest.TestCase):
     def setUp(self):
         # Create a sample DataFrame for testing
         data = {
-            'Feature1': [1, 2, 3, 4, 5],
-            'Feature2': [5, 4, 3, 2, 1],
-            'Feature3': [2, 2, 2, 2, 2],
-            'Feature4': [1, 1, 1, 1, 1]
+            "Feature1": [1, 2, 3, 4, 5],
+            "Feature2": [5, 4, 3, 2, 1],
+            "Feature3": [2, 2, 2, 2, 2],
+            "Feature4": [1, 1, 1, 1, 1],
         }
         self.sample_data = pd.DataFrame(data)
 
@@ -20,7 +24,7 @@ class TestFeatureSelectionMethods(unittest.TestCase):
         self.assertIsInstance(selected_data, pd.DataFrame)
 
         # Test if the correct features are selected based on correlation
-        expected_columns = ['Feature1', 'Feature3', 'Feature4']
+        expected_columns = ["Feature1", "Feature3", "Feature4"]
         self.assertCountEqual(selected_data.columns, expected_columns)
 
     def test_select_features_by_variance(self):
@@ -29,8 +33,9 @@ class TestFeatureSelectionMethods(unittest.TestCase):
         self.assertIsInstance(selected_data, pd.DataFrame)
 
         # Test if the correct features are selected based on variance
-        expected_columns = ['Feature1', 'Feature2']
+        expected_columns = ["Feature1", "Feature2"]
         self.assertCountEqual(selected_data.columns, expected_columns)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
